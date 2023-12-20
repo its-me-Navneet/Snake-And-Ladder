9031e638e557e3eef4876050a23f28ec7ddf8d18
navneet kumar <20bcs142@iiitdmj.ac.in>
2023-12-20 20:44:59 +0530

    Get the value between 1 to 6

--- src/App.java
+++ src/App.java
@@ -1,8 +1,13 @@
+import java.util.Random;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Welcome to snake and ladder game ");  
 
 
-        int pos =0;
+        int pos =0; 
+        
+        int value = new Random().nextInt(6) + 1; 
+            
     }
 }
