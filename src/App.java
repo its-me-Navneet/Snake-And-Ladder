9fa9345ef2a8be8b484806b70e5581ac79b01abf
navneet kumar <20bcs142@iiitdmj.ac.in>
2023-12-20 20:40:46 +0530

    initialise the game

--- src/App.java
+++ src/App.java
@@ -1,5 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Welcome to snake and ladder game "); 
+        System.out.println("Welcome to snake and ladder game ");  
+
+
+        int pos =0;
     }
 }
